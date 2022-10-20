e233c6853345ced4225cc0b071ba8068124b055d
christhian <christhian2524@gmail.com>
2022-10-19 22:17:21 -0500

    Add bcrypt for password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,36 @@
 import { request, response } from 'express';
+import bcrypt from 'bcrypt';
 import User from '../models/User.model.js';
+import { createError } from '../utils/error.js';
 
 export const register = async (req = request, res = response) => {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
   try {
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
   
     await newUser.save();
     res.status(200).send("User has been created");
+  } catch (error) {
+    next(error)
+  }
+}
+export const login = async (req = request, res = response, next) => {
+
+  try {
+    const user = await User.findOne( { username: req.body.username } );
+    if (!user) return next( createError(404, "User not found") );
+
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordCorrect) return next( createError(400, "Wrong password or username") );
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+
+    res.status(200).json(otherDetails);
   } catch (error) {
     next(error)
   }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,8 @@
-import express, { request, response } from 'express';
-import { register } from '../controllers/auth.js';
+import express from 'express';
+import { login, register } from '../controllers/auth.js';
 const router = express.Router();
 
 
-router.post("/", register);
+router.post("/register", register);
+router.post("/login", login);
 export default router;
